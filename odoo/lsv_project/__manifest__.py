95f0ff97e70678e022a587d9be5e96d1a262153f
Maryori Sabalza <maryorism730@gmail.com>
2021-07-30 02:57:24 +0000

    Upload New File

---
+++ odoo/lsv_project/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "LSV (Project) CAR-III",
+    'version':"1.0-dev1",
+    'summary': "My first module on Odoo, CAR-III 2021",
+    'author': "Maryori Sabalza Mejia",
+    'category':'WebSite',
+    'website': 'https://lsv-tech.com/car-2020/',
+    'depends': ['project', 'contacs', 'website_blog', 'website_slides', 'board'],
+    'data': {}, 
+    'installable': True,
+    'auto_install':False,
+    'license': "LGPL-3",
+
+}
